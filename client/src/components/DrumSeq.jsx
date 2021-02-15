c027f7562ffff41ec21472f8b1dd7dc9322fb6f4
Zak Hayek <zakhayek1@gmail.com>
2021-02-15 13:15:18 -0800

    Drum sequencer now plays notes in accordance with sequencer.

--- client/src/components/DrumSeq.jsx
+++ client/src/components/DrumSeq.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Tone from 'tone';
 
 class DrumSeq extends React.Component {
   constructor(props) {
@@ -13,12 +14,81 @@ class DrumSeq extends React.Component {
       
     };
     this.stepSwitch = this.stepSwitch.bind(this);
+    this.start = this.start.bind(this);
   }
 
   stepSwitch(col, row) {
     let sequence = this.state.sequence;
     sequence[col][row] = !sequence[col][row];
     this.setState({ sequence });
+  }
+
+  start() {
+    const notes = ['C5', 'C5', 'C5', 'C1'];
+    const steps = [];
+    const { sequence } = this.state;
+    for (let kit = 0; kit < sequence.length; kit++) {
+      let seq = [];
+      for (let hit = 0; hit < sequence[kit].length; hit++) {
+        if (sequence[kit][hit]) {
+          seq.push(notes[kit]);
+        } else {
+          seq.push(null);
+        }
+      }
+      steps.push(seq);
+    }
+    const hhOpen = new Tone.NoiseSynth().toDestination();
+    const hhClosed = new Tone.NoiseSynth().toDestination();
+    const snare = new Tone.NoiseSynth({
+      volume: 5,
+      noise: {
+        type: 'white',
+        playbackRate: 3,
+      },
+      envelope: {
+        attack: 0.001,
+        decay: 0.20,
+        sustain: 0.15,
+        release: 0.03,
+      },
+    }).toDestination();
+    const kick = new Tone.MembraneSynth().toDestination();
+
+    const hhOpenSeq = new Tone.Sequence(
+      function(time) {
+        hhOpen.triggerAttackRelease('4n', time);
+      },
+      steps[0],
+      '16n'
+    );
+    const hhClosedSeq = new Tone.Sequence(
+      function(time) {
+        hhClosed.triggerAttackRelease('4n', time);
+      },
+      steps[1],
+      '16n'
+    );
+    const snareSeq = new Tone.Sequence(
+      function(time) {
+        snare.triggerAttackRelease('4n', time);
+      },
+      steps[2],
+      '16n'
+    );
+    const kickSeq = new Tone.Sequence(
+      function(time, note) {
+        kick.triggerAttackRelease(note, 0.1, time);
+      },
+      steps[3],
+      '16n'
+    );
+    console.log(steps)
+    hhOpenSeq.start();
+    hhClosedSeq.start();
+    snareSeq.start();
+    kickSeq.start();
+    Tone.Transport.start();
   }
 
   render() {
@@ -28,6 +98,7 @@ class DrumSeq extends React.Component {
           {division.map((state, y) => (
             <button className={state ? `step_on` : `step_off`} key={x,y} name={`${x}${y}`} onClick={() => this.stepSwitch(x, y)}></button>
           ))}
+          <button onClick={this.start}>Test</button>
         </div>
       ))
     )
